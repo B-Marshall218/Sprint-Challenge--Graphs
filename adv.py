218f7de6b8127f79123ae42b0ba76c783fdae12b
Brian <lawmen218@yahoo.com>
2020-11-19 23:53:19 -0500

    getting there

--- adv.py
+++ adv.py
@@ -10,14 +10,14 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -26,11 +26,42 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
+# traversal_path = ['n', 'n']
+# traversal_path = ['n', 'w']
+# traversal_path = ['w', 'w']
+# traversal_path = ['s', 's']
+graph = {
 
+}
 
+directions = {"n": "s", "e": "w", "s": "n", "w": "e"}
 
+current_room = player.current_room.id
+
+exit_room = player.current_room.get_exits()
+
+graph[current_room] = {e: "?" for e in exit_room}
+
+# dft
+while "?" in graph[current_room].values():
+    t = random.choice([k for k, v in graph[current_room].items() if v == "?"])
+
+    prev_room = current_room
+    player.travel(t)
+    traversal_path.append(t)
+    current_room = player.current_room.id
+
+    if current_room not in graph:
+        graph[current_room] = {e: "?" for e in player.current_room.get_exits()}
+
+    graph[prev_room][t] = current_room
+    graph[current_room][directions[t]] = prev_room
+
+    if "?" not in graph[current_room].values():
+        # check which room youve visited
+        if len(graph) == 500:
+            print("End of the line")
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -41,11 +72,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
